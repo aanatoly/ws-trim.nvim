0722c4188796268a842c8ca780c5f6e917fa8cc4
Anatoly Asviyan <aanatoly@gmail.com>
2024-12-21 18:11:39 +0200

    support adding ws-trim to multiple conform fts

--- lua/ws-trim/init.lua
+++ lua/ws-trim/init.lua
@@ -1,11 +1,12 @@
 local M = {}
 
---- @class VenvOptions
 local defaults = {
   conform_fts = { "_" },
-  install_conform_fallback = true,
   max_blank_lines = 2,
   hl_group = { link = "Error" },
+  hl_name = "ExtraWhitespace",
+  hl_ws_prio = 10,
+  hl_clear_prio = 20,
 }
 
 M.options = {}
@@ -17,19 +18,20 @@ M.refresh = function(_)
     return
   end
   if rc2 then
-    local reg = "\\(^\\s*\\n\\)\\{%s}\\zs\\_s*\\n"
+    local reg = "\\S.*\\n\\zs\\(\\n\\)\\{1,%s}\\ze\\_s*\\S"
     reg = string.format(reg, M.options.max_blank_lines)
     vim.w.ws_enabled = {
       -- highlight spaces at the end of a line
-      vim.fn.matchadd("ExtraWhitespace", "\\s\\+$", 10),
-      -- highlight blank lines at the beggining of a file
-      vim.fn.matchadd("ExtraWhitespace", "\\%^\\_s*\\n", 10),
-      -- highlight blank lines at the end of a file
-      vim.fn.matchadd("ExtraWhitespace", "^\\_s*\\%$", 10),
-      -- highlight adjacent blank lines, starting from 3rd
-      vim.fn.matchadd("ExtraWhitespace", reg, 10),
-      -- -- no highlight for whitespaces before cursor position
-      vim.fn.matchadd("Normal", "\\s\\+\\%#", 20),
+      vim.fn.matchadd(M.options.hl_name, "\\s\\+$", M.options.hl_ws_prio),
+      -- no highlight for whitespaces before cursor position
+      vim.fn.matchadd("Normal", "\\s\\+\\%#", M.options.hl_clear_prio),
+
+      -- highlight all empty lines
+      vim.fn.matchadd(M.options.hl_name, "^\\n\\+", M.options.hl_ws_prio),
+      -- no highlight for first N blank lines, following text, unless EOF
+      vim.fn.matchadd("Normal", reg, M.options.hl_clear_prio),
+      -- no highlight for newline after cursor
+      vim.fn.matchadd("Normal", "\\%#\\n", M.options.hl_clear_prio),
     }
   else
     for _, r in ipairs(vim.w.ws_enabled) do
@@ -61,13 +63,34 @@ M.format = function(_, _, lines_in, callback)
   callback(nil, lines_out)
 end
 
+M.conform_setup = function()
+  local ok, conform = pcall(require, "conform")
+  if not ok then
+    return
+  end
+  conform.formatters["ws_trim"] = { format = M.format }
+  local fts = conform.formatters_by_ft
+  for _, ft in ipairs(M.options.conform_fts) do
+    -- print("ws ft: " .. ft)
+    local fmts = fts[ft]
+    if fmts == nil then
+      fmts = {}
+    elseif type(fmts) == "string" then
+      fmts = { fmts }
+    end
+    if type(fmts) ~= "table" then
+      error("Invalid fmts spec: " .. type(fmts))
+    else
+      table.insert(fmts, "ws_trim")
+      fts[ft] = fmts
+    end
+    -- print("conform fmts: " .. vim.inspect(fmts))
+  end
+end
+
 M.setup = function(options)
   M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
-  if M.options.install_conform_fallback then
-    require("conform").formatters_by_ft["_"] = { "ws_trim" }
-    require("conform").formatters["ws_trim"] = { format = M.format }
-  end
-
+  M.conform_setup()
   local augrp = vim.api.nvim_create_augroup("WhiteSpace", { clear = true })
   vim.api.nvim_create_autocmd({ "FileType" }, {
     group = augrp,
@@ -83,7 +106,7 @@ M.setup = function(options)
     end,
   })
 
-  vim.api.nvim_set_hl(0, "ExtraWhitespace", M.options.hl_group)
+  vim.api.nvim_set_hl(0, M.options.hl_name, M.options.hl_group)
 end
 
 return M
